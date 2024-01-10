e5942c5bae0a0ca467708e02e29bfafbef36711f
Ilan <smilan273@gmail.com>
2024-01-10 12:41:28 +0200

    updates

--- TA 2/src/Main.java
+++ TA 2/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -31,5 +33,46 @@ public class Main {
         System.out.println(obj.defaultField);
         obj.defaultMethod();
 
+        // Create an address
+        Address originalAddress = new Address("CityA", "Street1");
+
+        // Create a person with the original address
+        Person originalPerson = new Person("John", originalAddress);
+
+        // Create a person using the shallow copy constructor
+        Person shallowCopyPerson = new Person(originalPerson, false);
+
+        // Create a person using the deep copy constructor
+        Person deepCopyPerson = new Person(originalPerson, true);
+
+        // Modify the address in the original person
+        originalPerson.getAddress().setStreet("Street2");
+
+        // Print information
+        System.out.println("Original Person: " + originalPerson.getName() + ", " + originalPerson.getAddress().getStreet());
+        System.out.println("Shallow Copy Person: " + shallowCopyPerson.getName() + ", " + shallowCopyPerson.getAddress().getStreet());
+        System.out.println("Deep Copy Person: " + deepCopyPerson.getName() + ", " + deepCopyPerson.getAddress().getStreet());
+
+
+        // arrays
+        int[] original = {0,1,2,3,4};
+
+        // shallow copy
+        int[] ShallowCopy = original;
+
+        // deep copy
+        int[] deepCopy = new int[original.length];
+        for (int i = 0; i < deepCopy.length; i++) {
+            deepCopy[i] = original[i];
+        }
+
+        // update original
+        for (int i = 4; i > 0 ; i--) {
+            original[i] = i;
+        }
+
+        System.out.println("original: " + Arrays.toString(original));
+        System.out.println("shallow: " + Arrays.toString(ShallowCopy));
+        System.out.println("deep: "+ Arrays.toString(deepCopy));
     }
 }
